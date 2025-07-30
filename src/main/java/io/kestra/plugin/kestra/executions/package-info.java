cb189953da7715353930c73576fac4b395dca6c6
YannC. <ycoornaert@kestra.io>
2025-07-30 21:45:32 +0200

    fix: missing icons for query executions task

---
+++ src/main/java/io/kestra/plugin/kestra/executions/package-info.java
@@ -0,0 +1,4 @@
+@PluginSubGroup(title = "Kestra Executions", categories = PluginSubGroup.PluginCategory.TOOL)
+package io.kestra.plugin.kestra.executions;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
